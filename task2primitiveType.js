b0c48c78ee1a39a27e852a8cb23604ccf5f51fe0
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-08-24 15:05:26 +0400

    add task 2 primitive type coersion

---
+++ task2primitiveType.js
@@ -0,0 +1,17 @@
+//Task 2
+/*
+"" + 1 + 0 // Answer '10' ("" + 1 + 0 => "1" + 0 => "10")
+"" - 1 + 0 // Answer -1 ("" - 1 + 0 => 0 - 1 + 0 => -1)
+ true + false // Answer 1 (true + false => 1 + 0 => 1)
+ 6 / "3" // Answer 2 (6 / "3" => 6/3 => 2)
+"2" * "3"// Answer 6 ("2" * "3" => 2*3 => 2)
+4 + 5 + "px"// Answer "9px" (4 + 5 + "px" => 9 + "px" => "9px")
+"$" + 4 + 5// Answer "$45" ("$" + 4 + 5=> "$4"+5 => "$45")
+ "4" - 2 // Answer 2 ("4" - 2=> 4 - 2 => 2)
+"4px" - 2 // Answer NaN ("4px" - 2 => NaN -2 => NaN)
+7/0 // Answer Infinity
+ " -9 " + 5 // Answer "-95"(" -9 " + 5 => " -9 " + "5" => "-95")
+" -9 " - 5 // Answer -14 (" -9 " + 5 => -9  - 5 => -14)
+null + 1 //  Answer 1 (null + 1 => 0 + 1  => 1)
+undefined + 1 // Answer NaN (undefined + 1 => NaN + 1  => NaN)
+*/
